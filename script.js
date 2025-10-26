d0a140d804e62aae5fc0f31a8d6521ecf89fdb6d
DemianDyf <demiandyfort@gmail.com>
2025-10-26 20:13:22 -0300

    Generate a simpler version of the problem
    Let's just ask the computer to display the possible game options in the console (rock, paper, scissors)

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello");
+/* Simplest problem: get the console to display "rock", "paper" and "scissors"
+Create a function called getComputerChoice
+  Display message "Rock, Paper, Scissors"
+  Call function getComputerChoice  
+*/
+
+function getComputerChoice(){
+  console.log("Rock");
+  console.log("Paper");
+  console.log("Scissors");
+}
+
+getComputerChoice();
